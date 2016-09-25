9c1a40b5587d9dbcfc09f18bcc070a1bfe20f3bc
DanielSlater <danielslaterishere@gmail.com>
2016-09-25 14:00:11 +0100

    Added tests and fix to tic_tac_toe_x diagonal bug

--- tests/test_tic_tac_toe_x.py
+++ tests/test_tic_tac_toe_x.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
 
-from tic_tac_toe_x import has_winner, has_winning_line, play_game, random_player
+from tic_tac_toe_x import has_winner, _has_winning_line, play_game, random_player
 
 
 class TestTicTacToeX(TestCase):
 
     def test_has_winning_line(self):
-        self.assertEqual(1, has_winning_line((0, 1, 1, 1, 1), 4))
-        self.assertEqual(0, has_winning_line((0, 1, -1, 1, 1), 4))
-        self.assertEqual(1, has_winning_line((1, 1, 1, 1, 1, 0), 4))
-        self.assertEqual(0, has_winning_line((1, 1, 1, 1, 1, 0), 5))
-        self.assertEqual(-1, has_winning_line((-1, -1, -1, -1, 1), 4))
+        self.assertEqual(1, _has_winning_line((0, 1, 1, 1, 1), 4))
+        self.assertEqual(0, _has_winning_line((0, 1, -1, 1, 1), 4))
+        self.assertEqual(1, _has_winning_line((1, 1, 1, 1, 1, 0), 4))
+        self.assertEqual(1, _has_winning_line((1, 0, 1, 1, 1, 0), 3))
+        self.assertEqual(-1, _has_winning_line((-1, -1, -1, -1, 1), 4))
 
     def test_has_winner(self):
         board_state = ((0, 0, 0, 0, 0),
@@ -18,28 +18,28 @@ class TestTicTacToeX(TestCase):
                        (0, -1, 0, 0, 0),
                        (0, -1, 0, 0, 0),
                        (0, -1, 0, 0, 0))
-        self.assertEqual(-1, has_winner(board_state))
+        self.assertEqual(-1, has_winner(board_state, 4))
 
         board_state = ((0, 1, 0, 0, 0),
                        (0, 0, 1, 0, 0),
                        (0, 0, 0, 1, 0),
                        (0, 0, 0, 0, 1),
                        (0, 0, 0, 0, 0))
-        self.assertEqual(1, has_winner(board_state))
+        self.assertEqual(1, has_winner(board_state, 4))
 
         board_state = ((0, 0, 0, 0, 0),
                        (0, 0, 0, 0, 1),
                        (0, 0, 0, 1, 0),
                        (0, 0, 1, 0, 0),
                        (0, 1, 0, 0, 0))
-        self.assertEqual(1, has_winner(board_state))
+        self.assertEqual(1, has_winner(board_state, 4))
 
         board_state = ((0, 0, 0, -1, 0),
                        (0, 0, -1, 0, 0),
                        (0, -1, 0, 0, 0),
                        (-1, 0, 0, 0, 0),
                        (0, 0, 0, 0, 0))
-        self.assertEqual(-1, has_winner(board_state))
+        self.assertEqual(-1, has_winner(board_state, 4))
 
     def test_play_game(self):
         play_game(random_player, random_player)
