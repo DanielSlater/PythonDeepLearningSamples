d657d7bf07f3bd320821b1bc057b859563891612
DanielSlater <danielslaterishere@gmail.com>
2016-12-19 16:58:56 +0000

    rbm first version

---
+++ restricted_boltzmann_machine.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def create_rbm(input_tensor, hidden_nodes, learning_rate):
+    visible_nodes = int(input_tensor.get_shape()[-1])
+
+    weights = tf.Variable(tf.random_normal((visible_nodes, hidden_nodes), mean=0.0, stddev=1. / visible_nodes))
+    hidden_bias = tf.Variable(tf.zeros([hidden_nodes]))
+    visible_bias = tf.Variable(tf.zeros([visible_nodes]))
+
+    hidden_activation = tf.nn.sigmoid(tf.matmul(input_tensor, weights) + hidden_bias)
+    visible_reconstruction = tf.nn.sigmoid(tf.matmul(hidden_activation, tf.transpose(weights)) + visible_bias)
+
+    final_hidden_activation = tf.nn.sigmoid(tf.matmul(visible_reconstruction, weights) + hidden_bias)
+
+    positive = tf.matmul(tf.transpose(input_tensor), hidden_activation)
+    negative = tf.matmul(tf.transpose(visible_reconstruction), final_hidden_activation)
+
+    weight_update = weights.assign_add(learning_rate * (positive - negative))
+    visible_bias_update = visible_bias.assign_add(learning_rate *
+                                                  tf.reduce_mean(input_tensor - visible_reconstruction, 0))
+    hidden_bias_update = hidden_bias.assign_add(learning_rate *
+                                                 tf.reduce_mean(hidden_activation - final_hidden_activation, 0))
+
+    train_op = tf.group(weight_update, visible_bias_update, hidden_bias_update)
+
+    loss_op = tf.reduce_mean(tf.square(input_tensor - visible_reconstruction))
+
+    return hidden_activation, train_op, loss_op
+
+
+input_placeholder = tf.placeholder("float", shape=(None, 784))
+learning_rate = 0.01
+hidden_activation, train_op, loss_op = create_rbm(input_placeholder, hidden_nodes=300, learning_rate=learning_rate)
+
+mnist = input_data.read_data_sets("MNIST_data/")
+
+session = tf.Session()
+
+session.run(tf.initialize_all_variables())
+
+current_epochs = 0
+
+for i in range(20):
+    total_loss = 0
+    while mnist.train.epochs_completed == current_epochs:
+        batch_inputs, batch_labels = mnist.train.next_batch(100)
+        _, reconstruction_loss = session.run([train_op, loss_op], feed_dict={input_placeholder: batch_inputs})
+        total_loss += reconstruction_loss
+
+    print("epochs %s loss %s" % (current_epochs, reconstruction_loss))
+    current_epochs = mnist.train.epochs_completed
